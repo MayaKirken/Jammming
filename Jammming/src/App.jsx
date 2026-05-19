bdc2d549fca62779ed09e9ca1f80b0338191622d
Maya <twebgrl56@gmail.com>
2026-05-19 17:49:46 -0400

    Initialize searchResults state in App

--- Jammming/src/App.jsx
+++ Jammming/src/App.jsx
@@ -5,6 +5,11 @@ import SearchResults from './Components/SearchResults/SearchResults';
 import Playlist from './Components/Playlist/Playlist';
 
 function App() {
+  const [searchResults, setSearchResults] = useState([
+    { id: '1', name: 'Tiny Dancer', artist: 'Elton John', album: 'Madman Across the Water' }, 
+    { id: '2', name: 'Blue Monday', artist: 'New Order', album: 'Power, Corruption & Lies' },
+    { id: '3', name: 'Let It Happen', artist: 'Tame Impala', album: 'Currents' }
+  ]);
   return (
     <div>
       <h1>Ja<span className="highlight">mmm</span>ing</h1>
