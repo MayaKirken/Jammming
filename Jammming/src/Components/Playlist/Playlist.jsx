3d8ae2b5119024782d60ab159da7b6914f927d85
Maya <twebgrl56@gmail.com>
2026-05-19 11:07:11 -0400

    Build App layout and fix config rules

---
+++ Jammming/src/Components/Playlist/Playlist.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Playlist.css';
+
+function Playlist() {
+    return (
+        <div className="Playlist">
+            <input defaultValue={"New Playlist"} />
+            <button className="Playlist-save">SAVE TO SPOTIFIY</button>
+        </div>
+    );
+}
+
+export default Playlist;
