7b7b4b2ef5ad8277e7323d765ba14678e393382e
Maya <twebgrl56@gmail.com>
2026-05-19 17:26:59 -0400

    Build foundational TrackList component

---
+++ Jammming/src/Components/TrackList/TrackList.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './TrackList.css';
+import Track from '../Track/Track';
+
+function TrackList() {
+    return (
+        <div className="Tracklist">
+
+            <Track />
+            <Track />
+            <Track />
+        </div>
+    );
+}
+
+export default TrackList;
