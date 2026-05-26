62af2e3dd0c7fe4a9e396536e7d687996495c138
Maya <twebgrl56@gmail.com>
2026-05-26 17:34:30 -0400

    Add props to playlist in app.js

--- Jammming/src/App.jsx
+++ Jammming/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
         <SearchBar />
         <div className="App-playlist">
           <SearchResults searchResults={searchResults} />
-          <Playlist />
+          <Playlist playlistName={playlistName} playlistTracks={playlistTracks} />
         </div>
       </div>
     </div>
