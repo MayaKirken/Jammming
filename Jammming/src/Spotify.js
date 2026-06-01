0e46438ec024a333d03c96ab14540e56e1e0bd57
Maya <twebgrl56@gmail.com>
2026-06-01 18:12:25 -0400

    Finish Spotify auth and token extraction

--- Jammming/src/Spotify.js
+++ Jammming/src/Spotify.js
@@ -1,3 +1,5 @@
+const clientId = 'YOUR_ACTUAL_SPOTIFY_CLIENT_ID_HERE';
+const redirectUri = 'http://localhost:5173/';
 let accessToken;
 
 const Spotify = {
@@ -7,7 +9,21 @@ const Spotify = {
         }
         const accessTokenMatch = window.location.href.match(/access_token=[^&]*/);
         const expiresInMatch = window.location.href.match(/expires_token=[^&]*/);
+
+        if (accessTokenMatch && expiresInMatch) {
+        accessToken = accessTokenMatch[1];
+        const expiresIn = Number(expiresInMatch[1]);
+        window.setTimeout(() => accessToken = '', expiresIn * 1000);
+        window.history.pushState('Acesss Token', null, '/');
+        return accessToken;  
+        } else {
+            const accessUrl = 'https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}';
+            window.location = accessUrl;
+        }
+
     }
+
+
 }
 
 export default Spotify;
