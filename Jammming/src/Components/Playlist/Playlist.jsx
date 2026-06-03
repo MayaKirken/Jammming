c139afc783297b5eb4704065ae1c9debd2ec9abe
Maya <twebgrl56@gmail.com>
2026-06-03 16:42:22 -0400

    Fix playlist layout spacing and button typo

--- Jammming/src/Components/Playlist/Playlist.jsx
+++ Jammming/src/Components/Playlist/Playlist.jsx
@@ -10,7 +10,7 @@ function Playlist(props) {
         <div className="Playlist">
             <input value={props.playlistName} onChange={handleNameChange} />
             <TrackList tracks={props.playlistTracks} onRemove={props.onRemove} isRemoval={true} />
-            <button className="Playlist-save" onClick={props.onSave}>SAVE TO SPOTIFIY</button>
+            <button className="Playlist-save" onClick={props.onSave}>SAVE TO SPOTIFY</button>
         </div>
     );
 }
